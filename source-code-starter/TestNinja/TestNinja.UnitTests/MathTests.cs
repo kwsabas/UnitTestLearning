27421e58ca118b4ad7cdf17ef8568ac3a82806ee
kwsabas <kwsabas07@gmail.com>
2024-05-14 12:48:59 +0300

    feat: Implement half test for math class

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests {
+    [TestFixture]
+    public class MathTests {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArgumnets() {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumnetIsGreater_ReturnTheFirstArgument() {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumnetIsGreater_ReturnTheSecondArgument() {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument() {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+    }
+}
