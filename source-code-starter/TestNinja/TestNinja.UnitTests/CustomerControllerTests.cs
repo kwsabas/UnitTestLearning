dd8eb56ef92e3deb8adbacc50ff5b4bd44e9ebeb
kwsabas <kwsabas07@gmail.com>
2024-05-14 16:30:23 +0300

    feat: testing the return type of a method

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests {
+    [TestFixture]
+    public class CustomerControllerTests {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound() {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            // TypeOf ensures that is exactly that specific type
+            // NotFound
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // InstanceOf is used if we want to check also class derivatives
+            // NotFound or one of its derivatives
+            // Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk() {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
