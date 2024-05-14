a3c1e388df2eb2db607992a6dd5b00281081346d
kwsabas <kwsabas07@gmail.com>
2024-05-14 12:59:21 +0300

    feat: SetUp and TearDown Attributes

--- source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -10,38 +10,47 @@ using Math = TestNinja.Fundamentals.Math;
 namespace TestNinja.UnitTests {
     [TestFixture]
     public class MathTests {
+        private Math _math;
+
+        // SetUp
+        // Before the execution of each method
+        [SetUp]
+        public void SetUp() {
+            _math = new Math();
+        }
+
+        // TearDown - 
+        // After the execution of each method
+        // More possible at Integration Tests where maybe we want to clear things at the database
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArgumnets() {
-            var math = new Math();
 
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_FirstArgumnetIsGreater_ReturnTheFirstArgument() {
-            var math = new Math();
 
-            var result = math.Max(2, 1);
+            var result = _math.Max(2, 1);
 
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_SecondArgumnetIsGreater_ReturnTheSecondArgument() {
-            var math = new Math();
 
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
 
             Assert.That(result, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArgument() {
-            var math = new Math();
 
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
 
             Assert.That(result, Is.EqualTo(1));
         }
