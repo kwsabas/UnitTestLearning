c66f9d99bb3a14bb96b4df8ca7011c66be21ecc2
kwsabas <kwsabas07@gmail.com>
2024-05-15 10:56:16 +0300

    feat: NUnit test implementation for DemeriPointCalculator Class as exercise

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests {
+    [TestFixture]
+    public class DemeritPointsCalculatorTests {
+
+        private DemeritPointsCalculator _demeritPointsCalculator;
+
+        [SetUp]
+        public void SetUp() {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfBound_ThrowArgumentOutOfRangeException(int speed) {
+
+            Assert.That(() => _demeritPointsCalculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        [TestCase(80, 3)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedPoints) {
+
+            var points = _demeritPointsCalculator.CalculateDemeritPoints(speed);
+
+            Assert.That(points, Is.EqualTo(expectedPoints));
+        }
+
+    }
+}
