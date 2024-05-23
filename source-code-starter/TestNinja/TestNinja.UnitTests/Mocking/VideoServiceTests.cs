91ca279951a9ccfe83e2076715eadd93e929509c
kwsabas <kwsabas07@gmail.com>
2024-05-23 12:55:34 +0300

    feat: creating mock objects using moq for VideoService class

--- source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,15 +1,24 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking {
     [TestFixture]
     public class VideoServiceTests {
-        
+        private Mock<IFileReader> _fileReader;
+        private VideoService _service;
+
+        [SetUp]
+        public void SetUp() {
+            _fileReader = new Mock<IFileReader>();
+            _service = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError() {
-            var service = new VideoService(new FakeFileReader());
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
-            var result = service.ReadVideoTitle();
+            var result = _service.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
