bbf10e2473a243803e130585a1db910f728334c0
kwsabas <kwsabas07@gmail.com>
2024-05-23 12:31:06 +0300

    feat: Dependency Injection via parameters

--- source-code-starter/TestNinja/TestNinja/Mocking/VideoService.cs
+++ source-code-starter/TestNinja/TestNinja/Mocking/VideoService.cs
@@ -9,9 +9,15 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
-        public string ReadVideoTitle(IFileReader fileReader)
+        public IFileReader FileReader { get; set; }
+
+        public VideoService() {
+            FileReader = new FileReader();
+        }
+
+        public string ReadVideoTitle()
         {
-            var str = fileReader.Read("video.txt");
+            var str = FileReader.Read("video.txt");
             var video = JsonConvert.DeserializeObject<Video>(str);
 
             if (video == null)
